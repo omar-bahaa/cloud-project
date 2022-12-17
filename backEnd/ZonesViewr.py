cbb7a9431d88edf4395a1ae8df619a250d2b59d8
lamiaasamir <lamiiaasamiir@gmail.com>
2022-12-17 20:20:57 +0200

    added the classes for the viewer module

---
+++ backEnd/ZonesViewr.py
@@ -0,0 +1,71 @@
+from sas import SASConigurator
+class Viewer():
+    def __init__(self):
+        self.captureCommandsDictionary=[]
+        return 0
+    #def sendCommand(command, parameters):
+        #SASConigurator.send_command(command.format(parameters)) 
+
+    def showAlias(self):
+        self.command="show alias"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+    
+    def showDevices(self):
+        self.command="show device"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+
+    def showDomain(self):
+        self.command="show domain"
+        value = SASConigurator.send_command(command=self.command)  
+        self.captureCommandsDictionary.append(self.command)
+        return value
+
+    def showPhy(self):
+        self.command="show phy"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+
+    def showVersion(self):
+        self.command="show version"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+
+    def showZonegroup(self):
+        self.command="show zonegroup"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+
+    def showZoneset(self):
+        self.command="show zoneset"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+
+    def showZonesetData(self, zoneSetName):
+        self.command=f"show zoneset data {zoneSetName}"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+
+    def showLog(self):
+        self.command="show log"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+    def showInvalidtTot(self):
+        self.command="show invalidt2t"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
+    def showDiscoveryconfig(self):
+        self.command="show discoveryconfig"
+        value = SASConigurator.send_command(command=self.command) 
+        self.captureCommandsDictionary.append(self.command)
+        return value
