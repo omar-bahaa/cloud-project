bfdabdccf3db53796e411483e7df588c4fb22522
lamiaasamir <lamiiaasamiir@gmail.com>
2022-12-20 16:41:06 +0200

    edited files

--- Sas/ZonesConfig.py
+++ Sas/ZonesConfig.py
@@ -1,9 +1,8 @@
-from Sas.sas import SASConigurator
+from Sas.sas import SASManager
 from ZonesViewr import Viewer
 import re
 
-class ZoneGroup(SASConigurator):
-    allZonegroups=[]
+class ZoneGroup(SASManager):
     
     def __init__(self, zonegroupName):
         self.zonegroupName = zonegroupName
@@ -11,23 +10,14 @@ class ZoneGroup(SASConigurator):
         self.parentExpanderToPhysPorts = {} # expander is key and each value is a set of phys port numbers to which im connecting my device to that parent expander
         
         self.command = f"zonegroup create {self.zonegroupName}"
-        SASConigurator.sendAndCaptureCommand(self.command)
-        ZoneGroup.allZonegroups.append(self)
+        self.sendAndCaptureCommand(self.command)
     
-    
-    @classmethod
-    def deleteAllZoneGroups(cls):
-        command = "zonegroup delete all noconfirm"
-        SASConigurator.sendAndCaptureCommand(command)
-        for zonegroup in ZoneGroup.allZonegroups:
-            del zonegroup
-        ZoneGroup.allZonegroups=[]
         
     def renameZonegroup(self, newZoneGroupName):
         oldZoneGroupName = self.zonegroupName
         self.command = f"zonegroup rename {oldZoneGroupName} {newZoneGroupName}"
         self.zonegroupName = newZoneGroupName
-        SASConigurator.sendAndCaptureCommand(self.command)
+        self.sendAndCaptureCommand(self.command)
         return 0
 
     def addToZoneGroup(self, sasAddressToPhyNumberList: dict):
@@ -35,7 +25,7 @@ class ZoneGroup(SASConigurator):
             for phyNumber in phys:
                 self.parentExpanderToPhysPorts[expander].add(phyNumber)
                 self.command = f"zonegroup add {self.zonegroupName} {expander}:{phyNumber}"
-                SASConigurator.sendAndCaptureCommand(self.command)
+                self.sendAndCaptureCommand(self.command)
         return 0
     
     def removeDeviceFromZonegroup(self, sasAddressToPhyNumberList: dict):
@@ -43,53 +33,33 @@ class ZoneGroup(SASConigurator):
             for phyNumber in phys:
                 self.parentExpanderToPhysPorts[expander].remove(phyNumber)
                 self.command = f"zonegroup remove {self.zonegroupName} {expander}:{phyNumber}"
-                SASConigurator.sendAndCaptureCommand(self.command)
+                self.sendAndCaptureCommand(self.command)
         return 0
 
     
-    def _deleteZoneGroup(self):
-        self.command = f"zonegroup delete single {self.zonegroupName}"
-        SASConigurator.sendAndCaptureCommand(self.command)
-        ZoneGroup.allZonegroups.remove(ZoneGroup.allZonegroups.index(self))
-        return 0
+   
     
     def __del__(self):
         self._deleteZoneGroup()
         super().__del__()
 
 
-class ZoneSet(SASConigurator):
-    activeZoneset=""
-    allZonesets=set()
+class ZoneSet(SASManager):
+    
     
     def __init__(self, zonesetName):
         self.zonesetName = zonesetName
         self.captureCommandsList = []
         self.zonegroupPairsSetofSets = set()
-        
-        self.command = f"zoneset create {zonesetName}"
-        SASConigurator.sendAndCaptureCommand(self.command)
-        ZoneSet.allZonesets.add(self)
 
-    @classmethod   
-    def deactivateZoneset(cls):
-        command = "zoneset deactivate"
-        SASConigurator.sendAndCaptureCommand(command)
-        ZoneSet.activeZoneset=""
-        return 0
-        
-    @classmethod
-    def activateZoneset(self,zonesetName):
-        if ZoneSet.activeZoneset!="":
-            ZoneSet.deactivateZoneset()
-        self.command = f"zoneset activate {zonesetName}"
-        SASConigurator.sendAndCaptureCommand(self.command)
-        ZoneSet.activeZoneset = self.zonesetName    
-        return 0
+        self.command = f"zoneset create {zonesetName}"
+        self.sendAndCaptureCommand(self.command)
+
+    
     
     def addZoneGroupPairToZoneSet(self,zoneGroupA,zoneGroupB):
         self.command = f"zoneset add {self.zonesetName} {zoneGroupA} {zoneGroupB}"
-        SASConigurator.sendAndCaptureCommand(self.command)
+        self.sendAndCaptureCommand(self.command)
         self.zonegroupPairsSetofSets.add(frozenset(zoneGroupA,zoneGroupB))
         return 0
 
@@ -97,12 +67,12 @@ class ZoneSet(SASConigurator):
         oldZoneSetName = self.zonesetName
         self.zonesetName = newZonesetName
         self.command = f"zonegroup rename {oldZoneSetName} {newZonesetName}"
-        SASConigurator.sendAndCaptureCommand(self.command)
+        self.sendAndCaptureCommand(self.command)
         return 0
 
     def removeZonegroupPairFromZoneset(self, zoneGroupA, zoneGroupB):
         self.command = f"zoneset remove {self.zonesetName} {zoneGroupA} {zoneGroupB}"
-        SASConigurator.sendAndCaptureCommand(self.command)
+        self.sendAndCaptureCommand(self.command)
         self.zonegroupPairsSetofSets.remove(frozenset(zoneGroupA,zoneGroupB))
         return 0
 
@@ -110,7 +80,7 @@ class ZoneSet(SASConigurator):
 class Domain():
     pass
      
-class Device(SASConigurator):
+class Device(SASManager):
     def __init__(self, phy,aliasName=""):
         self.phyDictionary={"enabled":set(phy),"disabled":set()}
         self.aliasName=aliasName
@@ -120,12 +90,12 @@ class Device(SASConigurator):
     def createAlias(self, aliasName, sasAddress): #make sure alias must be unique each sas address has only one alias
         if self.aliasName=="":
             self.command = f"alias create {aliasName} {sasAddress} "
-        SASConigurator.sendAndCaptureCommand(self.command)
+        self.sendAndCaptureCommand(self.command)
             
     def disableDevice(self, sasAddress, phy):
         if phy in self.phyDictionary["enabled"]:
             self.command = f"device {sasAddress} {phy} disable"
-            SASConigurator.sendAndCaptureCommand(self.command)
+            self.sendAndCaptureCommand(self.command)
             self.phyDictionary["enabled"].remove(phy)
             self.phyDictionary["disabled"].add(phy)
         return 0
@@ -134,7 +104,7 @@ class Device(SASConigurator):
     def enableDevice(self,sasAddress,phy):
         if phy in self.phyDictionary["disabled"]:
             self.command = f"device {sasAddress} {phy} enable" 
-            SASConigurator.sendAndCaptureCommand(self.command)
+            self.sendAndCaptureCommand(self.command)
             self.phyDictionary["disabled"].remove(phy)
             self.phyDictionary["enabled"].add(phy)
         return 0
