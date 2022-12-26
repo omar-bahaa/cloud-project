133976d7d598a81670eb4f610004c5a754009165
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-12-26 20:31:06 +0200

    Create iscsi.py

---
+++ notes/iscsi.py
@@ -0,0 +1,10 @@
+#Discover targets at a given IP address:
+iscsi_discover_ip = lambda ip: f"iscsiadm --mode discoverydb --type sendtargets --portal {ip} --discover\n"
+#Login, must use a node record id found by the discovery:
+iscsi_login = lambda ip, portnum, discovered_ip: "iscsiadm --mode node --targetname {discovered_ip}:test --portal {ip}:{portnum} --login\n"
+#Logout
+iscsi_logout = lambda ip, portnum, discovered_ip: f"iscsiadm --mode node --targetname {discovered_ip}:test --portal {ip}:{portnum} --logout\n"
+#list node records
+iscsi_list_records = lambda : f"iscsiadm --mode node\n"
+# Display all data for a given node record
+iscsi_display_node = lambda ip, portnum, discovered_ip : f"iscsiadm --mode node --targetname {discovered_ip}:test --portal {ip}:{portnum}\n"
