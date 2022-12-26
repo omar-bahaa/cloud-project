d285cb32a33108320e5d9b911a7875cd87fd9441
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-12-26 20:41:51 +0200

    Update iscsi.py

--- notes/iscsi.py
+++ notes/iscsi.py
@@ -1,3 +1,14 @@
+'''
+creating ISCSI
+https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/storage_administration_guide/online-storage-management#target-setup-create-iscsi-target
+and everything after that 
+https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/storage_administration_guide/osm-create-iscsi-initiator
+API
+https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/storage_administration_guide/ch-iscsi#iscsi-api
+'''
+
+
+
 #Discover targets at a given IP address:
 iscsi_discover_ip = lambda ip: f"iscsiadm --mode discoverydb --type sendtargets --portal {ip} --discover\n"
 #Login, must use a node record id found by the discovery:
